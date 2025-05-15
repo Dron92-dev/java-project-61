cce5058f750ae4c5b81d2a00054a91b3577398bf
MacbookPro <macbookpro@MacBook-Pro-MacbookPro.local>
2025-05-15 18:09:48 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    application
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("java")
+    java
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}

6fdf0c20319d1a13e1e4ae66e90f8369a8fcbf4a
MacbookPro <macbookpro@MacBook-Pro-MacbookPro.local>
2025-06-09 16:06:06 +0300

    Add GitHub Actions workflow for SonarQube

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.51.0"
     id("java")
     java
+    id("org.sonarqube") version "6.2.0.5505"
 }
 
 group = "hexlet.code"
@@ -27,4 +28,12 @@ tasks.getByName<Test>("test") {
 
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Dron92-dev_java-project-61")
+        property("sonar.organization", "dron92-dev")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
